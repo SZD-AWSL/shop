8ab927923835cda840eb6bc7fc6bfa00cc644984
SZD-AWSL <registration_data@proton.me>
2024-04-11 17:30:25 -0400

    adding build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-
+set  CGO_ENABLED=0
 cd ./sportsstore
-go build -o ./app  main.go
+go build -v -o   .
+go run sportsstore
+#cd ./app/ && chmod +x main && ./main
+#go build -tags netgo -ldflags '-s -w' -o app
+
